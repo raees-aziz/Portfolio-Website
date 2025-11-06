3fea29115ca2f32018a37d52e88e4edbd5e5de49
raees-aziz <raees4963@gmail.com>
2025-11-06 19:47:51 +0500

    fix resume link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             Building Scalable Modern Websites for the Future
           </h2>
           <div className=" flex items-center gap-3">
-            <a href="https://drive.google.com/file/d/1TJRM49auS4ilqVLTHC0DzhZuRzV-t1v_/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/1X1hZn_V2vT4GuoFxgr5Hk05m3YeG6mMg/view?usp=sharing">
             <button className="btn flex btn-primary cursor-pointer text-zinc-700 font-bold">
               Dowload CV
               <img
